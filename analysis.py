b0897ea11f5db7587610178d2f5383b170f1b779
Ola Ipaye <ipayeola@outlook.com>
2024-05-14 23:57:30 +0100

    Updated my script file by making a copy of my dataset before cleaning it so I can work with the copy and not touch the original.

--- analysis.py
+++ analysis.py
@@ -6,8 +6,17 @@ import pandas as pd
 
 # Loading the first 5 rows of the dataset while using the read_csv function from the panda module
 data = pd.read_csv(".\data\women_clothing_ecommerce_sales.csv")
-print("Displaying the first 5 rows of dataset:")
-print(data.head())
+# print("Displaying the first 5 rows of dataset:")
+# print(data.head())
+
+# Create a copy of my dataset to work with
+data_copy = data.copy()
+print(data_copy.head())
 
 # Inspecting the dataset - Checking for missing values using the isnull() and sum() combo
-print(data.isnull().sum())
+# print("Number of missing values in each column:")
+# print(data.isnull().sum())
+
+# Fill missing 'size' values with the most frequent size
+# most_frequent_size = data['size'].mode()[0]
+# data['size'].fillna(most_frequent_size, inplace=True)
