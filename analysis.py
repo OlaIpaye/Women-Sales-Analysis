081debaf5b07c91bb0e8a92dd396c75d49ae766d
Ola Ipaye <ipayeola@outlook.com>
2024-05-14 23:26:24 +0100

    Updated the python script with a pandas function to inspect the dataset and check for missing values.

--- analysis.py
+++ analysis.py
@@ -1,5 +1,13 @@
 import pandas as pd
 
-# Using the read_csv function from the panda module to access the dataset
+# # Using the read_csv function from the panda module to access the dataset
+# data = pd.read_csv(".\data\women_clothing_ecommerce_sales.csv")
+# print(data.head())
+
+# Loading the first 5 rows of the dataset while using the read_csv function from the panda module
 data = pd.read_csv(".\data\women_clothing_ecommerce_sales.csv")
+print("Displaying the first 5 rows of dataset:")
 print(data.head())
+
+# Inspecting the dataset - Checking for missing values using the isnull() and sum() combo
+print(data.isnull().sum())
