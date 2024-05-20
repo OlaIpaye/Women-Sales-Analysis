96d3ec797ba3549917ed32261b2a10ece67b197c
Ola Ipaye <ipayeola@outlook.com>
2024-05-20 23:37:22 +0100

    Updated script file with the dtypes command to make sure all comuns in my daataset are in the appropriate format before i start to anaalyse them.

--- analysis.py
+++ analysis.py
@@ -12,11 +12,13 @@ data = pd.read_csv(".\data\women_clothing_ecommerce_sales.csv")
 # Create a copy of my dataset to work with
 data_copy = data.copy()
 print(data_copy.head())
+# print(data_copy.info())
 
 # Inspecting the dataset - Checking for missing values using the isnull() and sum() combo
 # print("Number of missing values in each column:")
-# print(data.isnull().sum())
+# print(data_copy.isnull().sum())
 
-# Fill missing 'size' values with the most frequent size
-# most_frequent_size = data['size'].mode()[0]
-# data['size'].fillna(most_frequent_size, inplace=True)
+# Using the dtypes command to check for the data types of each column in my dataframe - to ensure each column is stored in an appropriate format
+# print(data_copy.dtypes)
+
+
